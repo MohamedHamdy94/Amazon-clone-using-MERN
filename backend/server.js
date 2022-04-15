5be5ec5bcaf90950df7dfaa563cd6d32f9fe3ba4
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2022-04-15 21:47:51 +0200

    Publish-To-Heroku

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use("/api/orders", orderRouter);
 app.use("/api/admin", adminRouter);
 
 const __dirname = path.resolve()
-app.use(express.static(path.join(__dirname,'./admin/dist/admin')))
+app.use(express.static(path.join(__dirname,'./frontend/build')))
 app.get("*",(req , res)=>
-res.sendFile(path.join(__dirname,'./admin/dist/admin/index.html')))
+res.sendFile(path.join(__dirname,'./frontend/build/index.html')))
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
