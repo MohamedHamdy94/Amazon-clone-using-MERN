ba0f7f1d613ce7118523347f123802541b73ee44
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2022-04-16 10:35:13 +0200

    finshed project

---
+++ frontend/src/App.js
@@ -0,0 +1,149 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import { useContext, useEffect, useState } from "react";
+import { Store } from "./Store";
+import Badge from "react-bootstrap/esm/Badge";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import CartScreen from "./screens/CartScreen";
+import SigninScreen from "./screens/SigninScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import SignUpScreen from "./screens/SignupScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import Footer from "./components/Footer";
+import { getError } from "./utils";
+import axios from "axios";
+import SearchBox from "./components/SearchBox";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Toggle } from "./components/compon/Toggle";
+import { useDarkMode } from "./components/styles/useDarkMode";
+import { ThemeProvider } from "styled-components";
+import {
+  lightTheme,
+  darkTheme,
+  GlobalStyles,
+} from "./components/styles/GlobalStyles";
+
+function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+  console.log(cart.cartItems);
+  const signoutHandler = () => {
+    ctxDispatch({ type: "USER_SIGNOUT" });
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("shippingAddress");
+    localStorage.removeItem("paymentMethod");
+    window.location.href = "/signin";
+  };
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get("/api/products/categories");
+        console.log(data);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+  console.log(categories);
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={themeMode}>
+        <div
+          className={
+            sidebarIsOpen
+              ? "d-flex flex-column site-container active-cont"
+              : "d-flex flex-column site-container"
+          }
+        >
+          <GlobalStyles />
+          <ToastContainer position="bottom-center" limit={1} />
+          <header>
+            <Navbar bg="dark" variant="dark" expand="lg">
+              <Container>
+
+                <LinkContainer to="/">
+                  <Navbar.Brand>amazona</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Toggle theme={theme} toggleTheme={toggleTheme} />
+                  <Nav className="me-auto w-100 justify-content-end">
+                    <Link to="/cart" className="nav-link">
+                      cart
+                      {cart.cartItems.length > 0 && (
+                        <Badge pill bg="danger">
+                          {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                        </Badge>
+                      )}
+                    </Link>
+                    {userInfo ? (
+                      <NavDropdown
+                        title={userInfo.name}
+                        id="basic-nav-dropdown"
+                      >
+
+                        <Link
+                          className="dropdown-item"
+                          to="#signout"
+                          onClick={signoutHandler}
+                        >
+                          Sign Out
+                        </Link>
+                      </NavDropdown>
+                    ) : (
+                      <Link className="nav-link" to="/signin">
+                        Sign In
+                      </Link>
+                    )}
+
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          </header>
+          <main>
+            <Container className="mt-3">
+              <Routes>
+                <Route path="/product/:slug" element={<ProductScreen />} />
+                <Route path="/cart" element={<CartScreen />} />
+                <Route path="/signin" element={<SigninScreen />} />
+                <Route path="/signup" element={<SignUpScreen />} />
+                <Route path="/shipping" element={<ShippingAddressScreen />} />
+                <Route path="/payment" element={<PaymentScreen />} />
+                <Route path="/placeorder" element={<PlaceOrderScreen />} />
+                <Route
+                  path="/order/:id"
+                  element={
+                    <ProtectedRoute>
+                      <OrderScreen />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path={`/`} element={<HomeScreen />} />
+              </Routes>
+            </Container>
+          </main>
+          {/* <footer className="text-center">all reight reserved</footer> */}
+          <Footer className="mt-5" />
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HelmetProvider } from "react-helmet-async";
+import { StoreProvider } from "./Store";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+// import { Provider } from "reat-redux";
+// import { store } from "./Store/store";
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <StoreProvider>
+      {" "}
+      <HelmetProvider>
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
+      </HelmetProvider>
+    </StoreProvider>
+
+    {/* </Provider> */}
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
