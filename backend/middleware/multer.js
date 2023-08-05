19d2e64b5eff07d97740e8a1ac6005ab5e7d8893
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2023-08-05 23:07:50 +0300

    update the project

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 // File upload folder
-const DIR = 'assets';
+const DIR = '/assets/images';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
